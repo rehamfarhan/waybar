4615b654604174f68f8a225ebb1e5500f356c7d6
rehamfarhan <iamreham12345@gmail.com>
2026-03-02 21:32:46 +0600

    minor improvement to exams by (-1days) and wip sleep_warning.py

--- scripts/exams.py
+++ scripts/exams.py
@@ -3,7 +3,7 @@ import datetime
 import json
 
 def get_countdown():
-    exam_date = datetime.date(2026, 4, 21)
+    exam_date = datetime.date(2026, 4, 20)
     today = datetime.date.today()
     delta = exam_date - today
     days = delta.days
